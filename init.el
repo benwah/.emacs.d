f02875c8baac7a476114e447b0926cf46f42656d
Benoit Clennett-Sirois <bclennett-sirois@metrio.net>
2022-08-18 13:03:20 -0400

    Use straight-use-package instead of el-get

--- init.el
+++ init.el
@@ -7,9 +7,49 @@
 ;; Nothing worth mentioning.
 
 ;;; Packages:
-;; Note! Add packages to early-init.el
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(defun install-packages ()
+  (straight-use-package 'magit)
+  (straight-use-package 'elpy)
+  (straight-use-package 'flycheck)
+  (straight-use-package 'company-mode)
+  (straight-use-package 'exec-path-from-shell)
+  (straight-use-package 'markdown-mode)
+  (straight-use-package 'python-black)
+  (straight-use-package 'python-isort)
+  (straight-use-package 'python-pytest)
+  (straight-use-package 'poetry)
+  (straight-use-package 'rainbow-mode)
+  (straight-use-package 'smart-cursor-color)
+  (straight-use-package 'yaml-mode)
+  (straight-use-package 'helm)
+  (straight-use-package 'helm-ag)
+  (straight-use-package 'projectile)
+  (straight-use-package 'helm-projectile)
+  (straight-use-package 'js2-mode)
+  (straight-use-package 'jest)
+  (straight-use-package 'tide)
+  (straight-use-package 'web-mode)
+  (straight-use-package 'cython-mode)
+  (straight-use-package 'robe-mode)
+  (straight-use-package 'srcery-theme)
+  )
 
 (defun main ()
+  (install-packages)
   (init-emacs-ui)
   (init-key-bindings)
   (init-package-config)
@@ -31,6 +71,7 @@
   (desktop-save-mode -1)
   (electric-pair-mode -1)            ; Automagically close parenthesis / brackets.
   (delete-selection-mode 1)          ; Deletes content of marked-text when typing.
+  (menu-bar-mode -1)
   (show-paren-mode 1)                 ; Highlight matching parenthesis.
   (column-number-mode 1)              ; Display column number
   (tool-bar-mode -1)                  ; No toolbar
@@ -40,6 +81,7 @@
    js2-basic-offset 2
    js-indent-level 2)
   (setq
+   inhibit-startup-screen t
    require-final-newline nil
    make-backup-files -1
    auto-save-default -1
@@ -53,8 +95,8 @@
   (global-company-mode)
 
   ;; Theme
-  (load-theme 'cyberpunk t)
-  (set-face-attribute 'default nil :height 100 :family "Fira Code")
+  (load-theme 'srcery t)
+  (set-frame-parameter nil 'alpha-background 70)
   )
 
 
@@ -82,13 +124,7 @@
 ;; --------------------------
 (defun ben-comint-mode ()
   "Make sure windmove shortcuts work in comint-mode"
-  (add-hook 'comint-mode-hook
-            (function (lambda () 
-			(local-set-key (kbd "C-M-h") 'windmove-left)
-			(local-set-key (kbd "C-M-l") 'windmove-right)
-			(local-set-key (kbd "C-M-k") 'windmove-up)
-			(local-set-key (kbd "C-M-j") 'windmove-down))))
-)
+  )
 
 (defun ben-setup-projectile ()
   "Customize projectile"
@@ -102,11 +138,33 @@
 
 
 (defun ben-javascript-config ()
-  (setq auto-mode-alist (cons '("\\.js[mx]?\\'" . js2-mode) auto-mode-alist))
-  (add-hook 'js2-mode-hook
-	    (lambda () (interactive)
-	      (jest-minor-mode)
-	      (local-set-key (kbd "C-c t f") 'jest-popup))))
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode +1))
+
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+
+  ;; formats the buffer before saving
+  (add-hook 'before-save-hook 'tide-format-before-save)
+
+  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+  (require 'web-mode)
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+  (add-hook 'web-mode-hook
+	    (lambda ()
+	      (when (string-equal "tsx" (file-name-extension buffer-file-name))
+		(setup-tide-mode))))
+  )
 
 (defun ben-python-config ()
   (require 'poetry)
@@ -122,7 +180,6 @@
   (when (load "flycheck" t t)
     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
     (add-hook 'elpy-mode-hook 'flycheck-mode))
-
 
   (defun ben-python-hook ()
     ;; Settings
@@ -142,9 +199,8 @@
     (local-set-key (kbd "C-c t f") 'python-pytest-file)
     (local-set-key (kbd "C-c t t") 'python-pytest-function)
     )
+  (add-hook 'python-mode-hook 'ben-python-hook))
 
-  (add-hook 'python-mode-hook 'ben-python-hook)
-)
 
 (defun ben-setup-flycheck ()
   (global-flycheck-mode))
@@ -165,29 +221,41 @@
         (shell-command-to-string "wl-paste -n | tr -d \r")))
     (setq interprogram-cut-function 'wl-copy)
     (setq interprogram-paste-function 'wl-paste))
-)
+  )
+
+(defun ben-setup-misc ()
+  (smart-cursor-color-mode +1)
+
+  (add-hook 'css-mode-hook
+            (lambda()
+	      (rainbow-mode 1)
+	      (set-fill-column 120)
+	      (fci-mode t)))
+
+  (add-hook 'comint-mode-hook
+            (function (lambda () 
+		      (local-set-key (kbd "C-M-h") 'windmove-left)
+		      (local-set-key (kbd "C-M-l") 'windmove-right)
+		      (local-set-key (kbd "C-M-k") 'windmove-up)
+		      (local-set-key (kbd "C-M-j") 'windmove-down)))))
+
+
+(defun ben-setup-custom-functions ()
+  (defun close-all-buffers ()
+    (interactive)
+    (mapc 'kill-buffer (buffer-list)))
+  )
 
 (defun init-package-config ()
   "Configure individual packages"
   (exec-path-from-shell-initialize)
   (ben-setup-wl-clipboard)
   (ben-setup-projectile)
-  (ben-comint-mode)
   (ben-javascript-config)
   (ben-python-config)
-  (ben-setup-flycheck))
+  (ben-setup-flycheck)
+  (ben-setup-misc)
+  (ben-setup-custom-functions)
+  )
 
 (add-hook 'after-init-hook 'main)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(go-mode go-autocomplete tide helm-ag poetry cyberpunk-theme magit jest smart-cursor-color rainbow-mode python-pytest python-isort)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
